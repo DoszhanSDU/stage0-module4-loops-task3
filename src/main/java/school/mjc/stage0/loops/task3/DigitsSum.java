281d1175d9962a56848f9ac92a5778ec60f744e7
DosyaSDU <doszhan_turar@mail.ru>
2023-04-22 19:55:00 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0 ;
+        int len = String.valueOf(t).length();
+        for (int i = 0; i < len; i++) {
+            sum += t%10 ;
+            t /= 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int first = 1;
+        int second = 1;
+        if (lastFibonacci == 1) {
+            System.out.println(first);
+        } else if (lastFibonacci == 2) {
+            System.out.println(first);
+            System.out.println(second);
+        }else if (lastFibonacci > 2) {
+            System.out.println(first);
+            System.out.println(second);
+        for (int i = 3; i <= lastFibonacci; i++) {
+            int a = second ;
+            second = second + first ;
+            first = a ;
+            System.out.println(second);
+        }
+        }
     }
 }
